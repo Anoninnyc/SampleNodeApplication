354cc1d3e66a1a5e9acb49c0eca50f7eaceda037
Krishan Arya <krishanmarya1@gmail.com>
2016-11-28 22:05:52 -0700

    Code cleanup

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,8 +1,3 @@
 myApp = angular.module('myApp', ['ui.router', 'ngSanitize'])
-
 require('./config.js');
-
-
 require('./source/controllers/mainController.js');
-// require('./source/directives/directives.js');
-// require('./source/services/services.js');
